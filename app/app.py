7c5f8f65ddeaf1ac374dd8d7c0b1368aa0af4932
calcanal <calcanal@ttu.edu>
2023-11-21 00:03:20 -0600

    Some UI changes

--- app/app.py
+++ app/app.py
@@ -13,6 +13,18 @@ app.register_blueprint(semantic)
 def index():
     return render_template('index.html')
 
+@app.route('/chatbox/<int:chatbox_id>')
+def chatbox(chatbox_id):
+    return render_template('chatbox.html', chatbox_id = chatbox_id)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
 if __name__ == "__main__":
     try:
         app.run(host='0.0.0.0', port=5000, debug=True)
